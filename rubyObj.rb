89ace3cc84d776cb4a9c0d2ddc9138c9c49bd978
An Ngo <ngoan_230@yahoo.com>
2017-03-11 22:46:56 -0500

    initial commit

---
+++ rubyObj.rb
@@ -0,0 +1,72 @@
+# calculator
+
+class calculator
+	def addition(a, b)
+		sum = (a + b)
+		p sum
+		return sum
+	end
+
+	def substraction(a, b)
+		subtract = (a - b)
+		p subtract
+		return subtract
+	end
+
+	def multiplication(a, b)
+		multiply = (a * b)
+		p multiply
+		return multiply
+	end
+
+	def division(a, b)
+		divide = (a / b)
+		p divide
+		return divide
+	end
+end
+
+
+# Elevator
+class Elevator
+	attr_accessor :floor
+
+	def initialize
+		@floor = 1
+		p "Welcome! You are on the first floor."
+	end
+
+	def go_up
+		going_up
+	end
+
+	def go_down
+		going_down
+	end
+
+	def greeting
+		puts "You are now on the " + @floor + "."
+	end
+
+	def going_up
+		if @floor == 18
+			p "No more floors above this floor. Please choose another floor."
+		else
+			@floor -= 1
+			greeting
+		end
+
+	def going_down
+		if @floor == 1
+			p "No more floors below this floor. Please choose another floor"
+		else
+			@floor += 1
+			greeting
+		end
+	end
+end
+
+
+		
+
+
